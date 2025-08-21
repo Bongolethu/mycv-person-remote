7b8d91a7467a957a41b68dc375c273a0a6dbbd0c
Bongolethu <bongo@singds.co.za>
2025-08-21 22:47:51 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Person from "./components/Person"
+
+ 
+
+function App() { 
+  return (
+    <>
+      <div>
+        <Person />
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Person.tsx
@@ -0,0 +1,9 @@
+ 
+const Person: React.FC = () => { 
+   return( <div>
+      <h1> Remote Person </h1> 
+    </div>
+  );
+};
+
+export default Person;
